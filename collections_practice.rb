53754a97ad177bdb3dba1790324a728527b6d272
valeria campilongo <cvaleriac@hotmail.it>
2018-04-20 19:28:09 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,7 +15,7 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array, index, destination_index)
-  array.sort do |array1, array2|
+  array.sort do |array{1}, array{2}|
   array[2] <=> array [1]
   end
 end
